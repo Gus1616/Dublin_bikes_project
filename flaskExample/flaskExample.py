42c1b229e26fa730031f68400e176b10c96c1572
Gus1616 <90276026+Gus1616@users.noreply.github.com>
2022-03-11 23:39:15 +0000

    update to flask app 11/03

--- flaskExample/flaskExample.py
+++ flaskExample/flaskExample.py
@@ -1,3 +1,4 @@
+from distutils.log import debug
 from flask import Flask, render_template, g
 from sqlalchemy import create_engine
 from flask import jsonify
@@ -7,11 +8,11 @@ import json
 app = Flask(__name__)
 
 # connect to db
-URL = "database-1.cmd8vuwgew1e.us-east-1.rds.amazonaws.com"
+URL = "dublin-bikesdb.cmd8vuwgew1e.us-east-1.rds.amazonaws.com"
 PORT = "3306"
 DB = "dbikes"
 USER = "admin"
-PASSWORD = "Apples123"
+PASSWORD = "Dbikes123"
 
 engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(USER, PASSWORD, URL, PORT, DB), echo=True)
 
@@ -20,20 +21,33 @@ def home():
     return render_template('index.html')
 
 
-# @app.route("/")
-# def get_stations():
-#     engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(USER, PASSWORD, URL, PORT, DB), echo=True)
-#     sql = "select * from station;"
-#     rows = engine.execute(sql).fetchall()
-#     print('#found {} stations',len(rows))
-#     # return jsonify(stations=[dict(row.items()) for row in rows])
-#     df = pd.read_sql_table('station', engine)
+@app.route("/stations")
+def get_stations():
+    print("Calling stations")
+    # engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(USER, PASSWORD, URL, PORT, DB), echo=True)
+    # sql = "select * from station;"
+    df = pd.read_sql_table("station", engine)
+    # rows = engine.execute(sql).fetchall()
+    # print('#found {} stajtions',len(rows))
+    # return jsonify(stations=[dict(row.items()) for row in rows])
 
-#     results = df.to_json(orient='records')
-#     print(results)
-#     return results
+    results = df.to_json(orient='records')
+    # print(results)
+    return results
 
-# print(get_stations())
+@ app.route('/availability')
+def get_availability():
+    print("Calling availability")
+    # engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(USER, PASSWORD, URL, PORT, DB), echo=True)
+    # sql = "select * from station;"
+    df = pd.read_sql_table("availability", engine)
+    # rows = engine.execute(sql).fetchall()
+    # print('#found {} stajtions',len(rows))
+    # return jsonify(stations=[dict(row.items()) for row in rows])
+
+    results = df.to_json(orient='records')
+    # print(results)
+    return results
 
 if __name__== "__main__":
-    app.run()
+    app.run(debug=True)
