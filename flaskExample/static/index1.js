2ebc8abf2c089a939fea48ade2e1ad55d4dad9d0
Gus1616 <90276026+Gus1616@users.noreply.github.com>
2022-03-13 20:50:17 +0000

    adding sql query and about page link

--- flaskExample/static/index1.js
+++ flaskExample/static/index1.js
@@ -3,52 +3,44 @@ function initMap() {
         center: new google.maps.LatLng(53.350140, -6.266155),
         zoom: 14,
     });
+
+    infoWindow = new google.maps.InfoWindow();
+// code to get current location
+  const locationButton = document.createElement("button");
+
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+
+
+
     fetch("/stations").then(response => {
         return response.json();
     }).then((json) => {
-        // // console.log("bike stations", json)
-        // json.forEach(station => {
-        //     var color = 'Blue';
-        //     // console.log('HEllo')
-        //     fill1 = "<option value=";
-        //     fill2 = ">";
-        //     let text = "<select>";
-
-        //     // console.log(station.address);
-        //     text += fill1 + fill2 + station.address;
-
-        //     text += "</select>";
-        //     document.getElementById("textbox").innerHTML = text;
-
-
-        //     // console.log('station', station);
-        //     circle = new google.maps.Circle({
-        //         strokeColor: color,
-        //         strokeOpacity: '0.8',
-        //         strokeWeight: 0,
-        //         fillColor: color,
-        //         fillOpacity: 0.55,
-        //         map,
-        //         radius: 75,
-        //         clickable: true,
-        //         center: {
-        //             lat: station.position_lat,
-        //             lng: station.position_lng
-        //         },
-                
-        //     })
-        // var displayInfo = "<h3>" + station.address + "</br>" + station.position_lat + "," + station.position_lng;
-
-
-        // // Generate infoWindow
-        // makeClickable(map, circle, displayInfo);
-        // })        
-
-        fetch("/availability")
-          .then((response) => {
-            return response.json();
-          }).then(data=> {
-              // console.log("bike stations", json)
         json.forEach(station => {
             var color = 'Blue';
             // console.log('HEllo')
@@ -56,7 +48,7 @@ function initMap() {
             fill2 = ">";
             let text = "<select>";
 
-            // console.log(station.address);
+            // console.log(station);
             text += fill1 + fill2 + station.address;
 
             text += "</select>";
@@ -79,74 +71,22 @@ function initMap() {
                 },
                 
             })
-        var displayInfo = "<h3>" + station.address + "</br>" + station.position_lat + "," + station.position_lng;
+        var displayInfo = "<h3>" + station.address + "</br>" + station.position_lat + "," + station.position_lng +"</br>"+"</h3>Bikes Available : " + station.available_bikes + "</br>Bike Stands Free : " + station.available_bike_stands;
 
 
         // Generate infoWindow
         makeClickable(map, circle, displayInfo);
         })        
 
-
-        
-                // console.log("data ", data);
-                data.reverse()
-                console.log(data[0])
-                console.log("bike stations", json)
-
-                // console.log(" json", json);
+        fetch("/availability")
+          .then((response) => {
+            return response.json();
+          }).then(data=> {
+              
 
     });
 
 });
-}
-
-
-
-
-
-
-function locateStations(data, map, availability, stations) {
-    // data = response.json();
-    console.log("this is the data", data)
-    for (i = 0; i < 101; i++) {
-
-        var color = 'Blue';
-        
-        
-        console.log('HEllo')
-        fill1 = "<option value=";
-        fill2 = ">";
-        let text = "<select>";
-
-        console.log(data[i].address);
-        text += fill1 + fill2 + data[i].address;
-
-        text += "</select>";
-        document.getElementById("textbox").innerHTML = text;
-
-        // Properties for map circles
-        circle = new google.maps.Circle({
-            strokeColor: color,
-            strokeOpacity: '0.8',
-            strokeWeight: 0,
-            fillColor: color,
-            fillOpacity: 0.55,
-            map: map,
-            radius: 75,
-            clickable: true,
-            center: {
-                lat: data[i].position.lat,
-                lng: data[i].position.lng
-            },
-        });
-
-        
-        var displayInfo = "<h3>" + data[i].address + "</h3>Bikes Available : " + data[i].available_bikes + "</br>Bike Stands Free : " + data[i].available_bike_stands + "</br>" + data[i].position.lat + "," + data[i].position.lng;
-
-
-        // Generate infoWindow
-        makeClickable(map, circle, displayInfo);
-    }
 }
 
 
@@ -160,3 +100,15 @@ function makeClickable(map, circle, info) {
         infowindow.open(map);
     });
 }
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+      browserHasGeolocation
+        ? "Error: The Geolocation service failed."
+        : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(map);
+  }
+  
